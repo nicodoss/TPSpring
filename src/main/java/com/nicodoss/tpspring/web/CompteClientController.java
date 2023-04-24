c942a1c18e30ced178f7c3d5f8a3f43bd112d8b8
Nicodème AHLONSOU <nicodossadnk@gmail.com>
2023-04-24 20:53:09 +0100

    mise a jour des services

--- src/main/java/com/nicodoss/tpspring/web/CompteClientController.java
+++ src/main/java/com/nicodoss/tpspring/web/CompteClientController.java
@@ -2,25 +2,28 @@ package com.nicodoss.tpspring.web;
 
 import com.nicodoss.tpspring.dtos.CompteClientDto;
 import com.nicodoss.tpspring.enums.TypePersonne;
+import com.nicodoss.tpspring.exceptions.CompteClientNotExistException;
 import com.nicodoss.tpspring.services.CompteClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@RequestMapping("/clients")
 @RestController
 public class CompteClientController {
     @Autowired
     private  CompteClientService compteClientService;
-    @GetMapping(path = "/Clients/{TypePers}")
-    public List<CompteClientDto> GetClientByTypePersonne(@PathVariable("TypePers") TypePersonne typepers){
+
+    @GetMapping(path = "/{TypePers}")
+    public List<CompteClientDto> getClientsByTypePers(@PathVariable("TypePers") TypePersonne typepers){
         List<CompteClientDto>listeClient= new ArrayList<CompteClientDto>();
         compteClientService.getComptesByTypePersonnes(typepers);
         return listeClient;
+    }
+    @PutMapping("/{id}")
+    public CompteClientDto updateClient(@PathVariable Long id, @RequestBody CompteClientDto updatedClient) throws CompteClientNotExistException {
+        return compteClientService.UpdateCompteClients(id, updatedClient);
     }
 
 }
